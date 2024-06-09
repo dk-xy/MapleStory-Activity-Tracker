d6eb7b5fa59cde4bf86a7d9fe1c9dac85208b495
dk-xy <92148260+dk-xy@users.noreply.github.com>
2024-06-09 15:43:17 +0200

    edited logic showMiniProg

--- src/components/Legion/ShowMiniProg.jsx
+++ src/components/Legion/ShowMiniProg.jsx
@@ -1,12 +1,13 @@
 function ShowMiniProg ({ character }) {
     const quests = Object.values(character.progression.dailies.quests);
+    const activeQuests = quests.filter(quest => quest.isActive);
     const completedDailies = quests.reduce((count, quest) => {
         return quest.isActive && quest.completion.daily ? count + 1 : count;
     }, 0);
 
     return (
         <div>
-            dailies: {completedDailies} / {quests.length}
+            dailies: {completedDailies} / {activeQuests.length}
         </div>
     );
 }
