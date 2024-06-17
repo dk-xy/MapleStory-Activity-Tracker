2aa643f8ea4b33b4122e1f35bac1c8a204440920
dk-xy <92148260+dk-xy@users.noreply.github.com>
2024-06-17 10:38:51 +0200

    added symbol pictures

--- src/components/Legion/Progression/RegionCompletion.jsx
+++ src/components/Legion/Progression/RegionCompletion.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 // import progression css
 import './progression.css';
+import OblivionSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Vanishing_Journey.webp';
+import ChuChuSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Chuchu.webp';
+import LacheleinSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Lachelein.webp';
+import ArcanaSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Arcana.webp';
+import MorassSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Morass.webp';
+import EsferaSymbol from '../../../assets/symbols/Eqp_Arcane_Symbol_Esfera.webp';
+
+
+
+const symbolMap = {
+    'Oblivion': OblivionSymbol,
+    'Chu Chu': ChuChuSymbol,
+    'Lachelein': LacheleinSymbol,
+    'Arcana': ArcanaSymbol,
+    'Morass': MorassSymbol,
+    'Esfera': EsferaSymbol
+    // Add other region names and corresponding symbols here
+};
+
 
 function RegionCompletion({ region, characterId }) {
     const dispatch = useDispatch();
@@ -11,10 +30,11 @@ function RegionCompletion({ region, characterId }) {
     const handleCheckboxChange = (type) => {
         dispatch({ type: 'TOGGLE_COMPLETION', payload: { characterId, regionNameC: region.key, completionType: type } });
     };
-
+    const symbol = symbolMap[region.name];
     return (
         <div className={`regionProgression ${region.class}`}>
             <h5>{region.name}</h5>
+            {symbol && <img src={symbol} alt="Arcane Symbol" style={{position: 'absolute', top: 0, right: 0}}/>}
             <div className="completionBoxes">
                 <div className="regionCheckBox">
                     <label>Daily </label>
