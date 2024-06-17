343d3dae9d4c1dc4d9ef67b9fd18dafa57c46dfd
dk-xy <92148260+dk-xy@users.noreply.github.com>
2024-06-17 14:22:38 +0200

    added grandis symbols

--- src/components/Legion/Progression/GrandisCompletion.jsx
+++ src/components/Legion/Progression/GrandisCompletion.jsx
@@ -3,6 +3,24 @@ import { useDispatch } from 'react-redux';
 // import progression css
 import './progression.css';
 
+import CerniumSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_Cernium.webp';
+import ArcusSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_Arcus.webp';
+import OdiumSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_Odium.webp';
+import ShangriLaSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_ShangriLa.webp';
+import ArteriaSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_Arteria.webp';
+import CarcionSymbol from '../../../assets/symbols/Eqp_Sacred_Symbol_Carcion.webp';
+
+
+const symbolMap = {
+    'Cernium': CerniumSymbol,
+    'Arcus': ArcusSymbol,
+    'Odium': OdiumSymbol,
+    'Shangri La': ShangriLaSymbol,
+    'Arteria': ArteriaSymbol,
+    'Carcion': CarcionSymbol
+    // Add other region names and corresponding symbols here
+};
+
 function GradisCompletion({ region, characterId }) {
     const dispatch = useDispatch();
 
@@ -11,10 +29,11 @@ function GradisCompletion({ region, characterId }) {
     const handleCheckboxChange = (type) => {
         dispatch({ type: 'TOGGLE_COMPLETION', payload: { characterId, regionNameC: region.key, completionType: type } });
     };
-
+    const symbol = symbolMap[region.name];
     return (
         <div className={`regionProgression ${region.class}`}>
             <h5>{region.name}</h5>
+            {symbol && <img src={symbol} alt="Arcane Symbol" style={{position: 'absolute', top: 0, right: 0}}/>}
             <div className="completionBoxes">
                 <div className="regionCheckBox">
                     <label>Daily </label>
